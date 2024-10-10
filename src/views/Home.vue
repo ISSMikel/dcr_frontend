49939b61e15398602902d677454af4721e428b8e
ISSMikel <mikel.garcia@gov.tt>
2024-10-10 15:48:57 -0400

    Create Flag Function and Test Flagging, To Add Flag Icon Function and Change Buttons to Icons

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -267,14 +267,25 @@
                     <template
                         #item-navUrl="{ id, fileNumber, subject, receivedFrom, filing_area_id, correspondenceDate, dateReceived, sentDate, comments, flagged }">
                         <div class="flex space-x-2 items-center">
-                            <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="#000000" viewBox="0 0 256 256"><path d="M42.76,50A8,8,0,0,0,40,56V224a8,8,0,0,0,16,0V179.77c26.79-21.16,49.87-9.75,76.45,3.41,16.4,8.11,34.06,16.85,53,16.85,13.93,0,28.54-4.75,43.82-18a8,8,0,0,0,2.76-6V56A8,8,0,0,0,218.76,50c-28,24.23-51.72,12.49-79.21-1.12C111.07,34.76,78.78,18.79,42.76,50ZM216,172.25c-26.79,21.16-49.87,9.74-76.45-3.41-25-12.35-52.81-26.13-83.55-8.4V59.79c26.79-21.16,49.87-9.75,76.45,3.4,25,12.35,52.82,26.13,83.55,8.4Z"></path></svg>
-                            <button class="btn bg-green-400"
-                                @click="setcurrentCorrespondence({ id, fileNumber, subject, receivedFrom, filing_area_id, correspondenceDate, dateReceived, sentDate, comments, flagged })">Edit</button>
-
-                            <button class="btn bg-red-400" @click="deleteCorrespondenceInfo(id)">Delete</button>
-
                             
-                        </div>
+                            
+                            <svg v-if="flagged === 0" xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="#000000" viewBox="0 0 256 256"
+                            @click="updateCorrespondenceFlagged(id, fileNumber, subject, receivedFrom, filing_area_id, correspondenceDate, dateReceived, sentDate, comments, flagged)"
+                            ><path d="M42.76,50A8,8,0,0,0,40,56V224a8,8,0,0,0,16,0V179.77c26.79-21.16,49.87-9.75,76.45,3.41,16.4,8.11,34.06,16.85,53,16.85,13.93,0,28.54-4.75,43.82-18a8,8,0,0,0,2.76-6V56A8,8,0,0,0,218.76,50c-28,24.23-51.72,12.49-79.21-1.12C111.07,34.76,78.78,18.79,42.76,50ZM216,172.25c-26.79,21.16-49.87,9.74-76.45-3.41-25-12.35-52.81-26.13-83.55-8.4V59.79c26.79-21.16,49.87-9.75,76.45,3.4,25,12.35,52.82,26.13,83.55,8.4Z"></path></svg>
+                            
+                            <svg v-else="flagged === 1" xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="#c83c3c" viewBox="0 0 256 256"
+                            @click="updateCorrespondenceUnflagged(id, fileNumber, subject, receivedFrom, filing_area_id, correspondenceDate, dateReceived, sentDate, comments, flagged)"
+                            ><path d="M232,56V176a8,8,0,0,1-2.76,6c-15.28,13.23-29.89,18-43.82,18-18.91,0-36.57-8.74-53-16.85C105.87,170,82.79,158.61,56,179.77V224a8,8,0,0,1-16,0V56a8,8,0,0,1,2.77-6h0c36-31.18,68.31-15.21,96.79-1.12C167,62.46,190.79,74.2,218.76,50A8,8,0,0,1,232,56Z"></path></svg>
+                            
+                            <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="#365BEC" viewBox="0 0 256 256"
+                             @click="setcurrentCorrespondence({ id, fileNumber, subject, receivedFrom, filing_area_id, correspondenceDate, dateReceived, sentDate, comments, flagged })"
+                            ><path d="M227.32,73.37,182.63,28.69a16,16,0,0,0-22.63,0L36.69,152A15.86,15.86,0,0,0,32,163.31V208a16,16,0,0,0,16,16H216a8,8,0,0,0,0-16H115.32l112-112A16,16,0,0,0,227.32,73.37ZM136,75.31,152.69,92,68,176.69,51.31,160ZM48,208V179.31L76.69,208Zm48-3.31L79.32,188,164,103.31,180.69,120Zm96-96L147.32,64l24-24L216,84.69Z"></path></svg>
+                            
+                            <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="#c83c3c" viewBox="0 0 256 256"
+                            @click="deleteCorrespondenceInfo(id)"
+                            ><path d="M216,48H176V40a24,24,0,0,0-24-24H104A24,24,0,0,0,80,40v8H40a8,8,0,0,0,0,16h8V208a16,16,0,0,0,16,16H192a16,16,0,0,0,16-16V64h8a8,8,0,0,0,0-16ZM96,40a8,8,0,0,1,8-8h48a8,8,0,0,1,8,8v8H96Zm96,168H64V64H192ZM112,104v64a8,8,0,0,1-16,0V104a8,8,0,0,1,16,0Zm48,0v64a8,8,0,0,1-16,0V104a8,8,0,0,1,16,0Z"></path></svg>
+                          
+                        </div> 
                     </template>
 
                 </EasyDataTable>
@@ -334,7 +345,7 @@ export default {
                 const response = await axios.get(
                     `${import.meta.env.VITE_API_URL}/correspondence`, config
                 );
-                // console.log(response);
+                console.log(response);
                 this.correspondenceInfo = response.data.data
 
             } catch (e) {
@@ -430,10 +441,98 @@ export default {
 
         },
 
+        // Update Correspondence flagged
+        async updateCorrespondenceFlagged(id, fileNumber, subject, receivedFrom, filing_area_id, correspondenceDate, dateReceived, sentDate, comments, flagged) {
+            const { user } = useAuthStore();
+            const config = {
+                headers: {
+                    Authorization: `Bearer ${user.token}`,
+                },
+            };
+
+            const flaggedCorrespondence = {
+                id: id,
+                fileNumber: fileNumber,
+                subject: subject,
+                receivedFrom: receivedFrom,
+                sentDate: sentDate,
+                filing_area_id: filing_area_id,
+                correspondenceDate: correspondenceDate,
+                dateReceived: dateReceived,
+                comments: comments,
+                flagged: 1
+
+            }
+
+            console.log(flaggedCorrespondence)
+
+
+            try {
+                const response = await axios.put(
+                    `${import.meta.env.VITE_API_URL}/correspondence/${id}`,
+                     flaggedCorrespondence,
+                    config
+                );
+            
+          
+                
+                this.getCorrespondenceInfo()
+                // console.log(response)
+
+            } catch (e) {
+
+                console.log(e)
+            }
+        },
+
+         // Update Correspondence flagged
+         async updateCorrespondenceUnflagged(id, fileNumber, subject, receivedFrom, filing_area_id, correspondenceDate, dateReceived, sentDate, comments, flagged) {
+            const { user } = useAuthStore();
+            const config = {
+                headers: {
+                    Authorization: `Bearer ${user.token}`,
+                },
+            };
+
+            const unflaggedCorrespondence = {
+                id: id,
+                fileNumber: fileNumber,
+                subject: subject,
+                receivedFrom: receivedFrom,
+                sentDate: sentDate,
+                filing_area_id: filing_area_id,
+                correspondenceDate: correspondenceDate,
+                dateReceived: dateReceived,
+                comments: comments,
+                flagged: 0
+
+            }
+
+            console.log(unflaggedCorrespondence)
+
+
+            try {
+                const response = await axios.put(
+                    `${import.meta.env.VITE_API_URL}/correspondence/${id}`,
+                     unflaggedCorrespondence,
+                    config
+                );
+            
+          
+                
+                this.getCorrespondenceInfo()
+                // console.log(response)
+
+            } catch (e) {
+
+                console.log(e)
+            }
+        },
+
+
         // Update Correspondence Info
         async updateCorrespondenceInfo() {
             const { user } = useAuthStore();
-            console.log('Hello')
             const config = {
                 headers: {
                     Authorization: `Bearer ${user.token}`,
